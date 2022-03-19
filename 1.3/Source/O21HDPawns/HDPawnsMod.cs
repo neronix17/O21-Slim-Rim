5c39192953eed7f68682124e82ade9ccebbebcb2
neronix17 <39186138+neronix17@users.noreply.github.com>
2022-03-19 02:54:01 +0000

    a

--- 1.3/Source/O21HDPawns/HDPawnsMod.cs
+++ 1.3/Source/O21HDPawns/HDPawnsMod.cs
@@ -10,6 +10,8 @@ using Verse;
 
 using O21Toolbox;
 
+using HarmonyLib;
+
 namespace O21HDPawns
 {
     public class HDPawnsMod : Mod
@@ -21,12 +23,11 @@ namespace O21HDPawns
         {
             settings = GetSettings<HDPawnsSettings>();
             mod = this;
+
+            new Harmony("neronix17.slimrim.rimworld").PatchAll();
         }
 
-        public override string SettingsCategory()
-        {
-            return "Slim Rim";
-        }
+        public override string SettingsCategory() => "Slim Rim";
 
         public override void DoSettingsWindowContents(Rect inRect)
         {
@@ -40,9 +41,10 @@ namespace O21HDPawns
             base.DoSettingsWindowContents(inRect);
         }
     }
+
     public class HDPawnsSettings : ModSettings
     {
-        public bool slimRim = true;
+        //public bool slimRim = true;
         public bool slimRim_fat = true;
         public bool slimRim_hulk = false;
         public bool slimRim_thin = false;
@@ -52,7 +54,7 @@ namespace O21HDPawns
         {
             base.ExposeData();
 
-            Scribe_Values.Look(ref slimRim, "slimRim", true);
+            //Scribe_Values.Look(ref slimRim, "slimRim", true);
             Scribe_Values.Look(ref slimRim_fat, "slimRim_fat", true);
             Scribe_Values.Look(ref slimRim_hulk, "slimRim_hulk", false);
             Scribe_Values.Look(ref slimRim_thin, "slimRim_thin", false);
@@ -63,6 +65,23 @@ namespace O21HDPawns
             get
             {
                 return GetType().GetFields().Where(p => p.FieldType == typeof(bool) && (bool)p.GetValue(this)).Select(p => p.Name);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(PawnGenerator), "GenerateBodyType")]
+    public class PawnGenerator__GenerateBodyType
+    {
+
+        [HarmonyPostfix]
+        public static void Postfix(Pawn pawn)
+        {
+
+            if ((pawn.story.bodyType == BodyTypeDefOf.Thin && HDPawnsMod.settings.slimRim_thin) ||
+                (pawn.story.bodyType == BodyTypeDefOf.Fat && HDPawnsMod.settings.slimRim_fat) ||
+                (pawn.story.bodyType == BodyTypeDefOf.Hulk && HDPawnsMod.settings.slimRim_hulk))
+            {
+                pawn.story.bodyType = (pawn.gender == Gender.Female) ? BodyTypeDefOf.Female : BodyTypeDefOf.Male;
             }
         }
     }
